5a71fcaa4087898d0e682af54abb65af93c78729
Jonathan Melly <jonathan.melly@gmail.com>
2025-11-14 09:15:13 +0100

    feat(snapshots): store birthtime (crtime) if available (and restore it if possible)

--- snapshot/restore/local_fs_output_unix.go
+++ snapshot/restore/local_fs_output_unix.go
@@ -20,10 +20,24 @@ func symlinkChmod(path string, mode os.FileMode) error {
 	return nil
 }
 
-func symlinkChtimes(linkPath string, atime, mtime time.Time) error {
+func symlinkChtimes(linkPath string, btime, atime, mtime time.Time) error {
+	// Unix Lutimes only supports atime and mtime, birth time cannot be set on symlinks
 	//nolint:wrapcheck
 	return unix.Lutimes(linkPath, []unix.Timeval{
 		unix.NsecToTimeval(atime.UnixNano()),
 		unix.NsecToTimeval(mtime.UnixNano()),
 	})
+}
+
+func chtimes(path string, btime, atime, mtime time.Time) error {
+	// On Linux/FreeBSD, birth time cannot be set after file creation
+	// Fall back to standard os.Chtimes which sets atime/mtime
+	//nolint:wrapcheck
+	return os.Chtimes(path, atime, mtime)
+}
+
+// ChtimesExact is exported for testing purposes.
+// On Unix, birth time cannot be set, so this behaves the same as chtimes.
+func ChtimesExact(path string, btime, atime, mtime time.Time) error {
+	return chtimes(path, btime, atime, mtime)
 }
